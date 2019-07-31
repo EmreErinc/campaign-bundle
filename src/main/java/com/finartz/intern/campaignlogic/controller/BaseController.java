1e1b8d55c56c7903b8aac8d03993ca230ee080d8
emre <1306150016@ogr.iu.edu.tr>
2019-07-31 18:00:17 +0300

    cors added

--- src/main/java/com/finartz/intern/campaignlogic/controller/BaseController.java
+++ src/main/java/com/finartz/intern/campaignlogic/controller/BaseController.java
@@ -3,11 +3,14 @@ package com.finartz.intern.campaignlogic.controller;
 import com.finartz.intern.campaignlogic.security.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.Optional;
 
-import static com.finartz.intern.campaignlogic.security.SecurityConstants.*;
+import static com.finartz.intern.campaignlogic.security.SecurityConstants.HEADER_STRING;
+import static com.finartz.intern.campaignlogic.security.SecurityConstants.TOKEN_PREFIX;
 
+@CrossOrigin()
 public abstract class BaseController {
   @Autowired
   private JwtTokenProvider jwtTokenProvider;
